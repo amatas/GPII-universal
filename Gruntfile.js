ea87203fa873499160e10d0dcc6a6b7ff3236a2e
Alfredo Matas <amatas@gmail.com>
2016-05-26 20:58:01 +0200

    Added Grunt tasks to spin up the environments

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,12 +28,99 @@ module.exports = function (grunt) {
         },
         jsonlint: {
             src: ["gpii/**/*.json", "tests/**/*.json", "testData/**/*.json"]
-        }
+        },
+        run: {
+            productionup: {
+              	cmd: 'vagrant',
+                args: [
+                    'up',
+                    '--no-provision'
+                ],
+                options: {
+                    env: {
+                        HOME: process.env.HOME,
+                        VAGRANT_ENV: 'production-tests'
+                    }
+                }
+ 			},
+			productiontest: {
+              	cmd: 'vagrant',
+                args: [
+                    'provision'
+                ],
+                options: {
+                    env: {
+                        HOME: process.env.HOME,
+                        VAGRANT_ENV: 'production-tests',
+                        TEST_FILE: 'ProductionConfigTests.CI'
+                    }
+                }
+            },
+			productionstop: {
+              	cmd: 'vagrant',
+                args: [
+                    'destroy',
+                    '-f'
+                ],
+                options: {
+                    env: {
+                        HOME: process.env.HOME,
+                        VAGRANT_ENV: 'production-tests'
+                    }
+                }
+            },
+            vmup: {
+              	cmd: 'vagrant',
+                args: [
+                    'up',
+                    '--no-provision'
+                ],
+                options: {
+                    env: {
+                        HOME: process.env.HOME
+                    }
+                }
+ 			},
+			vmtest: {
+              	cmd: 'vagrant',
+                args: [
+                    'provision'
+                ],
+                options: {
+                    env: {
+                        HOME: process.env.HOME,
+                        TEST_FILE: 'ProductionConfigTests.CI'
+                    }
+                }
+            },
+			vmstop: {
+              	cmd: 'vagrant',
+                args: [
+                    'destroy',
+                    '-f'
+                ],
+                options: {
+                    env: {
+                        HOME: process.env.HOME
+                    }
+                }
+            },
+         }
+
     });
 
     grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-jsonlint");
     grunt.loadNpmTasks("grunt-shell");
     grunt.loadNpmTasks("grunt-gpii");
+    grunt.loadNpmTasks("grunt-run");
+
+    grunt.registerTask("test", "Run the tests of th GPII", function (n){
+		if (grunt.option("target")) {
+			grunt.task.run('dedupe-infusion','run:vm_tests');
+		} else {
+            grunt.task.run('dedupe-infusion','run:local_tests');
+		}
+    });
 
 };
